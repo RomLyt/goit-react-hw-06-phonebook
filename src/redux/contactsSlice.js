486c71128c13bbb7f949890a7296f610ec55981d
lytvynenkoroman84@gmail.com <121231829+RomLyt@users.noreply.github.com>
2023-12-23 12:17:22 +0200

    03

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -3,10 +3,10 @@ import { createSlice } from '@reduxjs/toolkit';
 //Початкове значення масива contacts у redux-стейті
 const initialState = {
   contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '+38-097-459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '+38-095-443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '+38-067-645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '+38-050-227-91-26' },
+    { id: 'id-1', name: 'Rosie Simpson', number: '097-459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '095-443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '067-645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '050-227-91-26' },
   ],
 };
 
